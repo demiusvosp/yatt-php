d2840a8a2d080e59803b5ad85a472053ddc6b064
Dmitriy demius Vospennikov <demius.vosp@gmail.com>
2017-09-27 17:46:19 +0300

    #158 Интеграция с AdminLTE. Добавил библу, переделал под неё основной шаблон.

---
+++ views/layouts/footer.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: demius
+ * Date: 27.09.17
+ * Time: 16:22
+ */
+
+?>
+<footer class="main-footer">
+    <div class="container-fluid">
+        <div class="pull-left">Yatt/php <?=Yii::$app->version ?></div>
+        <div class="pull-right hidden-xs">
+            <?= Yii::powered() ?>; <?= Yii::t('common', 'Design:')?> <a href="https://adminlte.io">AdminLTE</a>
+        </div>
+    </div>
+</footer>

5b13eda0b417140377cc01376a2414e5917cfa37
demius <demius.vosp@gmail.com>
2017-10-24 20:33:03 +0300

    переоформатировал роутинг

--- config/routes.php
+++ config/routes.php
@@ -2,46 +2,48 @@
 
 return [
     // authentication
-    'auth/login'         => 'authentication/login',
-    'auth/logout'        => 'authentication/logout',
-    'auth/registration'  => 'authentication/registration',
-    'auth/email-confirm' => 'authentication/confirm-email',
-    'auth/profile'       => 'authentication/profile',
-
-    // admin
-
-    // project managment
-    'admin/project/list'     => 'admin/project/index',
-    'admin/project/create'   => 'admin/project/create',
-    'admin/project/<id:\d+>/edit'     => 'admin/project/update',// внутренний маршрут не стоит того, чтобы переопределять кнопку в gridview
-    'admin/project/<id:\d+>/view'     => 'admin/project/view',
-    'admin/project/<id:\d+>/delete'   => 'admin/project/delete',
+    'auth/login'                             => 'authentication/login',
+    'auth/logout'                            => 'authentication/logout',
+    'auth/registration'                      => 'authentication/registration',
+    'auth/email-confirm'                     => 'authentication/confirm-email',
+    'auth/profile'                           => 'authentication/profile',
 
     // project
-    'p/<suffix:\w+>' => 'project/overview',
+    'p/<suffix:\w+>'                         => 'project/overview',
 
     // project setting
-    'p/<suffix:\w+>/setting'  =>  'project-settings/main',
-    'p/<suffix:\w+>/setting/stages' => 'project-settings/stages',
-    'p/<suffix:\w+>/setting/types'  => 'project-settings/types',
-    'p/<suffix:\w+>/setting/categories' => 'project-settings/categories',
-    'p/<suffix:\w+>/setting/versions'   => 'project-settings/versions',
-    'p/<suffix:\w+>/setting/difficulty' => 'project-settings/difficulties',
+    'p/<suffix:\w+>/setting'                 => 'project-settings/main',
+    'p/<suffix:\w+>/setting/stages'          => 'project-settings/stages',
+    'p/<suffix:\w+>/setting/types'           => 'project-settings/types',
+    'p/<suffix:\w+>/setting/categories'      => 'project-settings/categories',
+    'p/<suffix:\w+>/setting/versions'        => 'project-settings/versions',
+    'p/<suffix:\w+>/setting/difficulty'      => 'project-settings/difficulties',
 
     // события проекта
     // Справочники
     'DELETE p/<suffix:\w+>/dict/delete-item' => 'dict/delete-item',
 
     // task
-    'p/<suffix:\w+>/task/list'   => 'task/list',
-    'p/<suffix:\w+>/task/create' => 'task/create',
-    'p/<suffix:\w+>/task/<index:\d+>/edit' => 'task/edit',
-    'p/<suffix:\w+>/task/<index:\d+>'      => 'task/view',
-    'p/<suffix:\w+>/task/<index:\d+>/close' => 'task/close',
+    'task/<suffix:\w+>/list'                 => 'task/list',
+    'task/<suffix:\w+>/create'               => 'task/create',
+    'task/<suffix:\w+>-<index:\d+>'          => 'task/view',
+    'task/<suffix:\w+>-<index:\d+>/edit'     => 'task/edit',
+    'task/<suffix:\w+>-<index:\d+>/close'    => 'task/close',
+
+    // admin
+
+    // project managment
+    'admin/project/list'                     => 'admin/project/index',
+    'admin/project/create'                   => 'admin/project/create',
+    'admin/project/<id:\d+>/edit'            => 'admin/project/update',
+    // внутренний маршрут не стоит того, чтобы переопределять кнопку в gridview (во всяком случае пока)
+    'admin/project/<id:\d+>/view'            => 'admin/project/view',
+    'admin/project/<id:\d+>/delete'          => 'admin/project/delete',
+
 
     // main
-    'captcha' => 'main/captcha',
-    'error'   => 'main/error',
-    'about'   => 'main/about',
-    '/' => 'main/index',
+    'captcha'                                => 'main/captcha',
+    'error'                                  => 'main/error',
+    'about'                                  => 'main/about',
+    '/'                                      => 'main/index',
 ];

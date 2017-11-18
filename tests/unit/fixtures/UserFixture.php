566cfa0ca17e8ec131b6e19ce5b29ed439f57eff
demius <demius.vosp@gmail.com>
2017-11-18 16:49:30 +0300

    #212 - Сделать заполнение тестовыми данными. Скелет работы с фейкером (для тестов)

---
+++ tests/unit/fixtures/UserFixture.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * User: demius
+ * Date: 18.11.17
+ * Time: 16:21
+ */
+
+namespace tests\unit\fixtures;
+
+
+use yii\test\ActiveFixture;
+
+
+class UserFixture extends ActiveFixture
+{
+    public $modelClass = 'app\models\entities\User';
+}

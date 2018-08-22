ca13124f95cecd47200f6a83e4986b968506314b
demius <demius.vosp@gmail.com>
2018-08-22 15:41:30 +0300

    setting fix

--- config/params.php
+++ config/params.php
@@ -1,10 +1,11 @@
 <?php
 
 return [
-    'adminEmail' => 'admin@example.com',
-    'supportEmail' => 'support@example.com',
-    'user.torenExpire' => 3600*24*1, // время подтверждения сутки. Более чем достаточно
-    'user.rememberMeExpire' => 3600*24*30,
+    'adminEmail'            => 'admin@example.com',
+    'supportEmail'          => 'support@example.com',
+    'user.torenExpire'      => 3600 * 24 * 1, // время подтверждения сутки. Более чем достаточно
+    'user.rememberMeExpire' => 3600 * 24 * 30,
 
-    'defaultEditor' => 'plain',
+    'enableRegistration' => false,
+    'defaultEditor'      => 'plain',
 ];
